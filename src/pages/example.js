32647703b4b365eff04a6acf7d7a30d9e45c940d
hiroshi sato <thelastwezardof@gmail.com>
2022-10-10 15:04:29 +0900

    first commit

---
+++ src/pages/example.js
@@ -0,0 +1,15 @@
+import { useAuth } from '@/hooks/auth'
+
+const Example = () => {
+    const { logout, user } = useAuth({ middleware: 'auth' })
+
+    return (
+        <>
+            <p>{user?.name}</p>
+
+            <button onClick={logout}>Sign out</button>
+        </>
+    )
+}
+
+export default Example
